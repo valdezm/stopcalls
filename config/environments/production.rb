ca60b1f81c6759b076d94ef4d5e0517042f3c7bd
mark valdez <valdezm@uw.edu>
2012-01-03 13:32:59 -0800

    new user table mig

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,4 +57,20 @@ StopCalls5::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+  
+      config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  # config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+config.action_mailer.delivery_method = :smtp
+config.action_mailer.perform_deliveries = true
+config.action_mailer.raise_delivery_errors = true
+config.action_mailer.default :charset => "utf-8"
+
+ActionMailer::Base.smtp_settings = {
+  :address => "smtp.gmail.com",
+  :port => 587,
+  :authentication => :plain,
+  :domain => "gmail.com",
+  :user_name => "testemailvaldez76@gmail.com",
+  :password => "myPassword",
+}
 end
