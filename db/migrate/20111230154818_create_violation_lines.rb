10358e6b2875708dd9332d50c14e6a41e1432da4
mark valdez <valdezm@uw.edu>
2012-01-05 13:40:32 -0800

    new updates hopefully fix

--- db/migrate/20111230154818_create_violation_lines.rb
+++ db/migrate/20111230154818_create_violation_lines.rb
@@ -3,7 +3,7 @@ class CreateViolationLines < ActiveRecord::Migration
     create_table :violation_lines do |t|
       t.integer :company_id
       t.integer :user_id
-      t.integer :violationtype_id
+      t.integer :violation_type_id
       t.integer :violationlog_id
 
       t.timestamps
