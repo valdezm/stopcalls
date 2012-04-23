3541a8ef33d2b78352ae3b823e0ca8ee2dcd2691
mark valdez <valdezm@uw.edu>
2012-04-23 15:19:41 -0700

    added to title test

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,6 +33,7 @@ config.action_mailer.delivery_method = :smtp
 config.action_mailer.perform_deliveries = true
 config.action_mailer.raise_delivery_errors = true
 config.action_mailer.default :charset => "utf-8"
+config.log_level = :warn
 
 ActionMailer::Base.smtp_settings = {
   :address => "smtp.gmail.com",
